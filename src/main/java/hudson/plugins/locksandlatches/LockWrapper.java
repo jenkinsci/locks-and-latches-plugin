a14b67c32cfd299a69f3b8c4d3352421d90814d2
Seiji Sogabe <s.sogabe@gmail.com>
2011-07-14 23:38:18 +0900

    fixed compile error.
    - added import.

--- src/main/java/hudson/plugins/locksandlatches/LockWrapper.java
+++ src/main/java/hudson/plugins/locksandlatches/LockWrapper.java
@@ -38,6 +38,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.TimeUnit;
